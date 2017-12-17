26565cdd48dec43a176dde197313314673340187
keith <keith@raincityambience.com>
2017-12-16 20:19:30 -0800

    Style Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import '../css/login.css';
+import instaLogo from '../img/inst.png';
 
 var config = require('../config');
 
 
 class Login extends React.Component {
-  handleLogin(e){
-    e.preventDefault();
-    console.log("handleLogin");
-  }
+
 
   render() {
-    console.log(config.instaCred);
+
     return (
 
-      <div className="login-main">
-        <form action="" onSubmit={this.handleLogin}>
+      <div className="login-main" >
+        <form>
           <h1>Insta-Top</h1>
-          <a href={`https://api.instagram.com/oauth/authorize/?client_id=${config.instaCred.client_id}&redirect_uri=http://localhost:3000/top/&response_type=token&scope=likes+public_content`}>Login with Instagram</a>
+          <a className="button" href={`https://api.instagram.com/oauth/authorize/?client_id=${config.instaCred.client_id}&redirect_uri=http://localhost:3000/top/&response_type=token&scope=likes+public_content`}> <img src={instaLogo} alt="instagram logo"/> Login with Instagram</a>
         </form>
       </div>
     );
